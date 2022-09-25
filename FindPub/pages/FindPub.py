a0f86f05a218ea769b18aa69909e6798071cc336
CinthiyaMurugan <101140791+CinthiyaMurugan@users.noreply.github.com>
2022-09-26 01:39:31 +0530

    Update FindPub.py

--- FindPub/pages/FindPub.py
+++ FindPub/pages/FindPub.py
@@ -8,17 +8,43 @@ st.set_page_config(
 
 df = pd.read_csv('Data/open_pubs.csv')
 
-lat = st.sidebar.number_input(
-    "Latitude",
-    format="%.6f")
-long = st.sidebar.number_input('Longitude',
-    format="%.6f")
+def distance(x1, y1, x2, y2):
+    return ((x1 - x2) ** 2 + (y1 - y2) ** 2) ** ( 1 / 2)
+ 
+# Function to calculate K closest points
+def kClosest(points, target, K):
+    pts = []
+    n = len(points)
+    d = []
+ 
+    for i in range(n):
+        d.append({
+            "first": distance(points[i][0], points[i][1], target[0], target[1]),
+            "second": i
+        })
+     
+    d = sorted(d, key=lambda l:l["first"])
+    
+    for i in range(K):
+        pt = []
+        pt.append(points[d[i]["second"]][0])
+        pt.append(points[d[i]["second"]][1])
+        pts.append(pt)
+   
+# Calling DataFrame constructor on list
+    df_nearest_loc = pd.DataFrame(pts,columns=['latitude','longitude'])
+    st.subheader('Nearest five Pubs for the Latitude and longitude you entered')
+    st.map(df_nearest_loc)
+    
+# Driver code
+df_lat_log=df[['latitude','longitude']]
+points = df_lat_log.values.tolist()
 
+lat = st.sidebar.number_input('Enter Latitude',format="%.5f")
+log = st.sidebar.number_input('Enter Longitude',format="%.6f",key=int)
 
-city = df['local_authority'].unique().tolist()
-option = st.sidebar.selectbox("Select option", options=city)
+target = [lat,log]
+K = 5
 
-df_map = df.query('latitude >= @lat & longitude <= @long  & local_authority == @option')
-st.map(df_map.head(10))
-
+kClosest(points, target, K)
 
